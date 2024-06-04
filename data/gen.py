100e8ca08d4d93c8806dcdca51f3a93e609005ef
anajlm <anaj.limartins@gmail.com>
2024-06-03 22:16:09 -0300

    Removing unnecessary code

--- data/gen.py
+++ data/gen.py
@@ -1,40 +1,15 @@
-# import pandas as pd
-# import numpy as np
-
-# # Configurações
-# num_disciplinas = 100
-# num_salas = 100
-
-# # Geração de dados para disciplinas
-# disciplinas = {
-#     'N': np.random.randint(20, 60, size=num_disciplinas)
-# }
-
-# disciplinas_df = pd.DataFrame(disciplinas)
-# disciplinas_df.to_csv('disciplinas.csv', index=False)
-
-# # Geração de dados para salas
-# salas = {
-#     'C': np.random.randint(30, 70, size=num_salas),
-#     'D': np.random.randint(50, 200, size=num_disciplinas)
-# }
-
-# salas_df = pd.DataFrame(salas)
-# salas_df.to_csv('salas.csv', index=False)
-
-# disciplinas_df.head(), salas_df.head()
+## Script for generating random data for the problems's input variables ##
 
 import pandas as pd
 import numpy as np
 
-# Configurations
+# Defining the size of the problem
 num_disciplinas = 100
 num_salas = 100
-num_predios = 5  # Number of buildings
 
 # Generating data for room capacity
 capacidade_salas = {
-    'Capacidade': np.random.randint(30, 70, size=num_salas)  # Random capacity for each room
+    'Capacidade': np.random.randint(30, 70, size=num_salas)  # Capacity for each room
 }
 capacidade_salas_df = pd.DataFrame(capacidade_salas)
 capacidade_salas_df.index = np.arange(1, len(capacidade_salas_df) + 1)  # Starting index from 1
@@ -55,13 +30,3 @@ distancia_salas = {
 distancia_salas_df = pd.DataFrame(distancia_salas)
 distancia_salas_df.index = np.arange(1, len(distancia_salas_df) + 1)  # Starting index from 1
 distancia_salas_df.to_csv('distancia_salas.csv', index_label='Sala')
-
-# # Displaying head of the generated files
-# print("Head of capacidade_salas.csv:")
-# print(capacidade_salas_df.head())
-
-# print("\nHead of alunos_por_disciplina.csv:")
-# print(alunos_por_disciplina_df.head())
-
-# print("\nHead of distancia_salas.csv:")
-# print(distancia_salas_df.head())
--- src/relax_and_fix.py
+++ src/relax_and_fix.py
@@ -2,61 +2,17 @@ import numpy as np
 import random
 from gurobipy import *
 
-# Set the environment variable to point to your license file
-#os.environ["GRB_LICENSE_FILE"] =  "C:\Users\LP2G\gurobi.lic"
-
 class RelaxAndFix:
     def __init__(self, N, C, D, num_disciplines, num_rooms, verbose=True):
-        self.N = N  # Número de estudantes por disciplina
-        self.C = C  # Capacidade das salas
-        self.D = D  # Distancia das salas
+        self.N = N  # Number of students per class
+        self.C = C  # Capacity for each room
+        self.D = D  # Distance from each room to the building of each discipline 
         self.num_disciplines = num_disciplines # Number of disciplines
         self.num_rooms = num_rooms # Number of classrooms
         self.verbose = verbose
-        
-    def order_ascending(array):
-        # Crie uma lista de tuplas (índice, elemento)
-        indexed_array = [(index, array[index]) for index, element in enumerate(array)]
-
-        # Ordene a lista de tuplas com base no valor do elemento em ordem crescente
-        sorted_indexed_array = sorted(indexed_array, key=lambda x: x[1])
-
-        # Extraia os índices da lista de tuplas ordenadas
-        sorted_indexes = [index for index, element in sorted_indexed_array]
-        
-        return sorted_indexes
-
-    def order_descending(array):
-        # Create a list of tuples (index, element)
-        indexed_array = [(index, array[index]) for index, element in enumerate(array)]
-
-        # Sort the list of tuples based on element value in descending order
-        sorted_indexed_array = sorted(indexed_array, key=lambda x: x[1], reverse=True)
-
-        # Extract the indexes from the sorted list of tuples
-        sorted_indexes = [index for index, element in sorted_indexed_array]
-        
-        return sorted_indexes
-
-    def get_initial_solution(self, N, C):
-        # Ordenar as disciplinas e salas pela quantidade de alunos e distancia das salas
-        # As salas com menor distancia sao atribuidas as turmas com maior numero de alunos,
-        # de modo a minimizar a funcao objetivo
-        N_sorted = self.order_descending(N)
-        #print(N_sorted)
-        #D_sorted = self.order_ascending(D)
-        C_sorted = self.order_descending(C)
-        #print(D_sorted)
-
-        # Initialize the solution (x) matrix
-        initial_solution = np.zeros((self.num_disciplines, self.num_rooms), dtype=int)
-        for i in range(len(N)):
-            initial_solution[N_sorted[i], C_sorted[i]] = 1
-            
-        return initial_solution
-
-    def add_input_variables(self, d, s):
-        self.model = Model(name='Room Allocation Problem')
+    
+    def _add_input_variables(self, d, s):
+        self.model = Model(name='Classroom Assignment Problem')
         self.model.setParam('TimeLimit', 10)
         
         x = {}
@@ -67,7 +23,7 @@ class RelaxAndFix:
         
         return x
     
-    def add_constraints(self, C, N, x, d, s):
+    def _add_constraints(self, C, N, x, d, s):
         # Constraint 1: Each room is assigned to exactly one discipline
         for j in range(1, s + 1):
             self.model.addConstr(quicksum(x[i][j] for i in range(1, d + 1)) == 1)
@@ -81,7 +37,7 @@ class RelaxAndFix:
             for j in range(1, s + 1):
                 self.model.addConstr(N[i] * x[i][j] <= C[j])
 
-    def define_objective_function(self, x, D, N, d, s):
+    def _define_objective_function(self, x, D, N, d, s):
         self.model.setObjective(
             quicksum(x[i][j] * D[i][j] * N[i]
                  for i in range(1, d + 1)
@@ -89,7 +45,7 @@ class RelaxAndFix:
             sense=GRB.MINIMIZE
         )
         
-    def relax_and_fix(self, x, num_disciplines, num_rooms):
+    def _relax_and_fix(self, x, num_disciplines, num_rooms):
         # Stage 1: Relax and optimize for the first third of the disciplines
         if self.verbose:
             print("First iteration of relax and fix...")
@@ -111,7 +67,7 @@ class RelaxAndFix:
                 if i <= round(0.3 * num_disciplines):
                     self._fix_variable(x[i][j], x[i][j].X)
                 elif i <= round(0.6 * num_disciplines):
-                    self._unfix_variable(x[i][j])
+                    self._unrelax_variable(x[i][j], GRB.INTEGER)
                 else:
                     self._relax_variable(x[i][j])
         
@@ -126,7 +82,7 @@ class RelaxAndFix:
                 if i <= round(0.6 * num_disciplines):
                     self._fix_variable(x[i][j], x[i][j].X)
                 else:
-                    self._unfix_variable(x[i][j])
+                    self._unrelax_variable(x[i][j], GRB.INTEGER)
         
         self.model.optimize()
         self._found_a_valid_solution()
@@ -146,6 +102,12 @@ class RelaxAndFix:
         var.lb = 0
         var.ub = 1
         return var
+    
+    def _unrelax_variable(self, var, vtype, lower_default=0):
+        var.vtype = vtype
+        var.lb = lower_default  # Reset lower bound to default (0)
+        var.ub = float('inf')  # Reset upper bound to default (unconstrained)
+        return var
 
     def _found_a_valid_solution(self):
         if self.model.SolCount > 0:
@@ -157,30 +119,12 @@ class RelaxAndFix:
         else:
             print("Error: it was not able to find a valid solution")
             exit(1)
-        
-
-    def optimize(self, initial_solution, iterations=1000):
-        # Copiar a solução inicial
-        best_solution = initial_solution.copy()
-        best_cost = self.objective_function(best_solution)
-        
-        for _ in range(iterations):
-            # Realizar um swap randômico
-            new_solution, _ = self.swap_random_values(best_solution.copy())
-            new_cost = self.objective_function(new_solution)
-            
-            # Verificar se a nova solução é melhor
-            if new_cost < best_cost:
-                best_solution = new_solution
-                best_cost = new_cost
-        
-        return best_solution
 
     def compute_solution(self):
-        x = self.add_input_variables(self.num_disciplines, self.num_rooms)
-        self.add_constraints(self.C, self.N, x, self.num_disciplines, self.num_rooms)
-        self.define_objective_function(x, self.D, self.N, self.num_disciplines, self.num_rooms)
-        self.relax_and_fix(x, self.num_disciplines, self.num_rooms)
+        x = self._add_input_variables(self.num_disciplines, self.num_rooms)
+        self._add_constraints(self.C, self.N, x, self.num_disciplines, self.num_rooms)
+        self._define_objective_function(x, self.D, self.N, self.num_disciplines, self.num_rooms)
+        self._relax_and_fix(x, self.num_disciplines, self.num_rooms)
         
         for v in self.model.getVars():
             print(f"{v.VarName} {v.X:g}")
