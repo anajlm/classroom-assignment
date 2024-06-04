c059ee8dc773d14b354e1e308a9e6a16ae7fd864
anajlm <anaj.limartins@gmail.com>
2024-06-03 22:16:09 -0300

    Removing unnecessary code

--- src/main.py
+++ src/main.py
@@ -4,18 +4,8 @@ import pandas as pd
 import numpy as np
 import csv
 
-
-
-# # Read data from CSV files
-# alunos_por_disciplina_df = pd.read_csv('..\data\disciplina.csv', index_col='Disciplina')
-# # salas_df = pd.read_csv('..\data\salas.csv', header=0)
-
-# alunos_por_disciplina_array = alunos_por_disciplina_df.values.flatten()
-# print(alunos_por_disciplina_array)
-
-
-# Read data from the CSV file and pad the arrays to handle one-based indexing
-# Ler dados do arquivo de capacidades das salas
+# Read data from the CSV files and store them in dictionaries
+# Reading room capacity data
 C = {}
 with open('..\data\capacidade_salas.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar=';')
@@ -24,7 +14,7 @@ with open('..\data\capacidade_salas.csv', newline='') as csvfile:
         room = int(row[0])
         C[room] = int(row[1])
 
-# Ler dados do arquivo de número de alunos das disciplinas
+# Reading number of students per discipline data
 N = {}
 with open('..\data\disciplinas.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar=';')
@@ -33,62 +23,29 @@ with open('..\data\disciplinas.csv', newline='') as csvfile:
         discipline = int(row[0])
         N[discipline] = int(row[1])
         
-# Ler dados do arquivo de distancia das salas
+# Reading distance of each classroom from the building of each discipline data
 D = {}
 with open('..\data\distancia_salas.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar=';')
     next(reader)
     for row in reader:
-        room = int(row[0])  # Room number (first column)
-        distances = list(map(int, row[1:]))  # Distances (remaining columns)
+        room = int(row[0])
+        distances = list(map(int, row[1:]))
         # Create a dictionary for the distances with discipline numbers as keys
         D[room] = {i + 1: distances[i] for i in range(len(distances))}
 
-# print(C)
-# print(D[1])
-# print(N)
-# Display the first few entries to verify
-# print("First few entries in the dictionary D:")
-# for key in list(D.keys())[:5]:
-#     print(f"Sala {key}: {D[key]}")
 
-# N = disciplinas_df['N'].values
-# C = salas_df['C'].values
-# D = salas_df['D'].values
-
-# sa = simulated_annealing.SimulatedAnnealing()
+# Get a initial solution by appling Relax-and-Fix
 raf = relax_and_fix.RelaxAndFix(N, C, D, 100, 100)
 initial_solution = raf.compute_solution()
-
 print(initial_solution)
+
+# Use the initial solution to obtain the optimized solution with Simulated Annealing
+# sa = simulated_annealing.SimulatedAnnealing()
+#optimized_solution = sa.compute_solutuion()
 
 # neighbor = sa.solution_to_neighbor(initial_solution, D_sorted)
 # print(neighbor)
 
 # solution = sa.neighbor_to_solution(neighbor)
 # print(np.array_equal(solution,initial_solution))
-
-# swaped = sa.get_neighbor(neighbor)
-# print(swaped)
-
-# Ensure each classroom is ocupied by a class
-# for i in range(len(N)):
-#     j = np.random.randint(0, len(C))
-#     initial_solution[i, j] = 1
-    
-#optimal_solution = simulated_annealing.optimize(N, C, initial_solution)
-
-# Dumps solution on CSV file
-
-
-   
-# # Exemplo de uso
-# N = np.random.randint(20, 60, size=100)  # Número de estudantes por disciplina
-# C = np.random.randint(30, 70, size=100)  # Capacidade das salas
-
-# raf = RelaxAndFix(N, C)
-# initial_solution = raf.fix()
-# optimized_solution = raf.optimize(initial_solution)
-
-# print("Solução inicial:", initial_solution)
-# print("Solução otimizad
